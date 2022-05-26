d7a91b9960d7ad86ae7767060f405b9405f2537f
afelix997 <afelix2013@gmail.com>
2022-05-25 18:19:54 -0700

    new

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,9 @@
+import math
 def to_roman(num):
-    # write your code here!
+    romans_dict = {'M': 1000, 'CM': 900,'D': 500,'CD':400,'C': 100,'XC': 90,'L': 50,'XL': 40,'X': 10,'IX': 9,'V': 5,'IV': 4,'I': 1}
+    answer= []
+    for roman in romans_dict:
+        arab= math.floor(num/int(romans_dict[roman]))
+        num-= arab * romans_dict[roman]
+        answer.append(roman*arab)
+    return "".join(answer)
